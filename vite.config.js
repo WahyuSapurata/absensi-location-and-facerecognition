ea0a86e1f253ec3df637845aad5b55430b200b66
WahyuSapurata <wm337708@gmail.com>
2024-08-08 23:48:48 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/face-api.js'],
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/build', // Sesuaikan jika Anda menggunakan direktori build yang berbeda
-    },
 });
